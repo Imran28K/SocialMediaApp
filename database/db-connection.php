f1ae4735de9a4b8617ce1f656bd5fd98c4707239
Imran28K <imran28may@gmail.com>
2024-04-27 22:23:01 +0100

    front-end login page and database development

---
+++ database/db-connection.php
@@ -0,0 +1,15 @@
+<?php
+$host = '127.0.0.1'; 
+$user = 'root'; 
+$pass = ''; 
+$db = 'socialmediadb'; 
+
+$mysqli = new mysqli($host, $user, $pass, $db);
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+
+echo "Connected successfully!";
+
+?>
